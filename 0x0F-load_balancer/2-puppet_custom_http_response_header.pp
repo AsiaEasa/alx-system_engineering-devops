e2d311d62aa7739aec6df0177121de8415ba6ee9
AsiaEasa <asiaeasa89@gmail.com>
2024-03-30 08:21:03 -0700

    done

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,27 @@
+# 2-puppet_custom_http_response_header.pp
+
+# Install Nginx package
+package { 'nginx':
+  ensure => installed,
+}
+
+# Define Nginx service
+service { 'nginx':
+  ensure => running,
+  enable => true,
+  require => Package['nginx'],
+}
+
+# Add custom HTTP header to Nginx configuration
+file { '/etc/nginx/conf.d/custom_headers.conf':
+  ensure  => present,
+  content => "add_header X-Served-By $hostname;\n",
+  notify  => Service['nginx'],
+}
+
+# Create a simple index page
+file { '/usr/share/nginx/html/index.html':
+  ensure  => present,
+  content => "Hello World!\n",
+  notify  => Service['nginx'],
+}
