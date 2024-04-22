6df31730b8e719b1c03284928204b3c4003962fc
AsiaEasa <asiaeasa89@gmail.com>
2024-04-22 11:09:44 -0700

    done

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,28 +3,31 @@
 a python script that using this REST API, for a given employee ID,
 returns information about his/her TODO list progress.
 """
-import re
 import requests
-from sys import argv
+import sys
 
+def fetch_todo_progress(employee_id):
+    url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+    response = requests.get(url)
+    
+    if response.status_code != 200:
+        print("Failed to fetch data from the API.")
+        return
+    
+    todos = response.json()
+    total_tasks = len(todos)
+    completed_tasks = [todo for todo in todos if todo['completed']]
+    num_completed_tasks = len(completed_tasks)
+    employee_name = todos[0]['name']
+    
+    print(f"Employee {employee_name} is done with tasks ({num_completed_tasks}/{total_tasks}):")
+    for task in completed_tasks:
+        print(f"\t{task['title']}")
 
 if __name__ == "__main__":
-    API = "https://jsonplaceholder.typicode.com"
-    if re.fullmatch(r'\d+', argv[1]):
-        id = int(argv[1])
-
-        usr_json = requests.get(f'{API}/users/{id}').json()
-        todos_json = requests.get(f'{API}/todos').json()
-
-        user_name = usr_json.get('name')
-        usr_todos = [task for task in todos_json if task.get('userId') == id]
-        todos_done = [task for task in usr_todos if task.get('completed')]
-
-        print('Employee {} is done with tasks({}/{}):'.format(user_name,
-                                                              len(todos_done),
-                                                              len(usr_todos)
-                                                              )
-              )
-
-        for task in todos_done:
-            print('\t {}'.format(task.get('title')))
+    if len(sys.argv) != 2:
+        print("Usage: python script_name.py employee_id")
+        sys.exit(1)
+    
+    employee_id = int(sys.argv[1])
+    fetch_todo_progress(employee_id)
