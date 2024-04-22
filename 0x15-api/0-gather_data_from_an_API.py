1c47981512b922f02d25d4ce938a96e28d4378f5
AsiaEasa <asiaeasa89@gmail.com>
2024-04-22 11:24:23 -0700

    done

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,27 +1,41 @@
 #!/usr/bin/python3
-'''
-Python script that returns information using REST API
-'''
+"""
+a python script that using this REST API, for a given employee ID,
+returns information about his/her TODO list progress.
+"""
+
 import requests
 from sys import argv
 
+def get_user_completed_tasks(user_id):
+    base_url = "https://jsonplaceholder.typicode.com/"
+
+    user_request = requests.get("{}users/{}".format(base_url, user_id))
+    user_info = user_request.json()
+    user_name = user_info.get("name")
+
+    if user_name is not None:
+        todos_request = requests.get("{}todos?userId={}".format(base_url, user_id))
+        todos_info = todos_request.json()
+
+        all_tasks = len(todos_info)
+        completed_tasks = [task for task in todos_info if task.get("completed")]
+        completed_count = len(completed_tasks)
+
+        return user_name, completed_count, all_tasks, completed_tasks
+    else:
+        return None
+
 if __name__ == "__main__":
     if len(argv) > 1:
-        user = argv[1]
-        url = "https://jsonplaceholder.typicode.com/"
-        req = requests.get("{}users/{}".format(url, user))
-        name = req.json().get("name")
-        if name is not None:
-            jreq = requests.get(
-                "{}todos?userId={}".format(
-                    url, user)).json()
-            alltsk = len(jreq)
-            completedtsk = []
-            for t in jreq:
-                if t.get("completed") is True:
-                    completedtsk.append(t)
-            count = len(completedtsk)
-            print("Employee {} is done with tasks({}/{}):"
-                  .format(name, count, alltsk))
-            for title in completedtsk:
-                print("\t {}".format(title.get("title")))
+        user_id = argv[1]
+        user_info = get_user_completed_tasks(user_id)
+
+        if user_info:
+            user_name, completed_count, all_tasks, completed_tasks = user_info
+            print("Employee {} is done with tasks({}/{}):".format(user_name, completed_count, all_tasks))
+
+            for completed_task in completed_tasks:
+                print("\t{}".format(completed_task.get("title")))
+        else:
+            print("User not found.")
