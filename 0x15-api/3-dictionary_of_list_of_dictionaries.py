4e1aeac13459e35e334064d8444e4c22d258249b
AsiaEasa <asiaeasa89@gmail.com>
2024-04-22 12:53:10 -0700

    done

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Python script that returns information using REST API"""
+import csv
+import json
+import requests
+import sys
+
+
+def fetch_all_todo_lists():
+    URL = "https://jsonplaceholder.typicode.com/"
+
+    ALL_USERS = requests.get(URL + "users").json()
+
+    ALL_TASKS = {}
+
+    for USER in ALL_USERS:
+        USER_ID = USER["id"]
+        USERNAME = USER["username"]
+        TODOS = requests.get(URL + "todos", params={"userId": USER_ID}).json()
+        USER_TASKS = [
+            {"username": USERNAME, "task": t.get("title"), "completed": t.get("completed")}
+            for t in TODOS
+        ]
+        ALL_TASKS[USER_ID] = USER_TASKS
+
+    with open("todo_all_employees.json", "w") as JSONFILE:
+        json.dump(ALL_TASKS, JSONFILE, indent=4)
+
+
+if __name__ == "__main__":
+    fetch_all_todo_lists()
