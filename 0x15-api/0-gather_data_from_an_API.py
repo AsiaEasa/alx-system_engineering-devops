07600dd1f608bf67b22d8a967b4f37746a839cbc
AsiaEasa <asiaeasa89@gmail.com>
2024-04-22 11:04:23 -0700

    done

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,33 +4,35 @@ a python script that using this REST API, for a given employee ID,
 returns information about his/her TODO list progress.
 """
 import requests
-import sys
+from sys import argv
+
 
 def fetch_todo_progress(employee_id):
-    base_url = 'https://jsonplaceholder.typicode.com/users'
-    todo_url = f'{base_url}/{employee_id}/todos'
-
-    try:
-        response = requests.get(todo_url)
-        response.raise_for_status()
-        todos = response.json()
+    url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    response = requests.get(url)
+    
+    if response.status_code != 200:
+        print("Failed to fetch data from the API.")
+        return
+    
+    name = response.json().get("name")
+    if name is not None:
+        url_todos = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+        todos_response = requests.get(url_todos)
+        todos = todos_response.json()
         
-        completed_tasks = [todo['title'] for todo in todos if todo['completed']]
         total_tasks = len(todos)
+        completed_tasks = [todo for todo in todos if todo['completed']]
         num_completed_tasks = len(completed_tasks)
-        employee_name = todos[0]['name']  # Use 'name' field for employee name
         
-        print(f"Employee {employee_name} is done with tasks({num_completed_tasks}/{total_tasks}):")
+        print(f"Employee {name} is done with tasks ({num_completed_tasks}/{total_tasks}):")
         for task in completed_tasks:
-            print(f"\t{task}")
-
-    except requests.exceptions.RequestException as e:
-        print("Error:", e)
+            print(f"\t{task['title']}")
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
-        sys.exit(1)
-
-    employee_id = sys.argv[1]
+    if len(argv) != 2:
+        print("Usage: python script_name.py employee_id")
+        exit(1)
+    
+    employee_id = argv[1]
     fetch_todo_progress(employee_id)
