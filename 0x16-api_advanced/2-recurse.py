7c4aa8f5268101c656adf2e9f7be1430d6769f35
AsiaEasa <asiaeasa89@gmail.com>
2024-05-07 10:40:03 -0700

    done

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,20 +4,20 @@ import requests
 
 
 def recurse(subreddit, hot_list=[], after=None):
-    """queries the Reddit API"""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=100"
-    headers = {'User-Agent': 'Hi/0.'}
-    params = {'after': after} if after else {}
+    """Queries the Reddit API"""
+    URL = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=100"
+    HEADERS = {'User-Agent': 'Hi/0.'}
+    PARAMS = {'after': after} if after else {}
 
-    r = requests.get(url, headers=headers, params=params)
-    if r.status_code == 200:
-        data = r.json()
-        posts = data['data']['children']
-        if posts:
-            hot_list.extend([post['data']['title'] for post in posts])
-            after = data['data']['after']
-            if after:
-                return recurse(subreddit, hot_list, after)
+    R = requests.get(URL, headers=HEADERS, params=PARAMS)
+    if R.status_code == 200:
+        DATA = R.json()
+        POSTS = DATA['data']['children']
+        if POSTS:
+            hot_list.extend([post['data']['title'] for post in POSTS])
+            AFTER = DATA['data']['after']
+            if AFTER:
+                return recurse(subreddit, hot_list, AFTER)
             else:
                 return hot_list
         else:
