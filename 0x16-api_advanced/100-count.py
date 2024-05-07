7bc89f33ce445f5fa41444e5881bffe6d93a50aa
AsiaEasa <asiaeasa89@gmail.com>
2024-05-07 10:22:03 -0700

    done

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -3,12 +3,13 @@
 
 import requests
 
+
 def count_words(subreddit, word_list, after=None, counts=None):
     if counts is None:
         counts = {}
-    
+
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=100"
-    headers = {'User-Agent': 'Your Custom User-Agent Here'}
+    headers = {'User-Agent': 'Hi/0.0'}
     params = {'after': after} if after else {}
 
     response = requests.get(url, headers=headers, params=params)
@@ -19,7 +20,9 @@ def count_words(subreddit, word_list, after=None, counts=None):
             title = post['data']['title'].lower()
             for word in word_list:
                 if word.lower() in title:
-                    counts[word.lower()] = counts.get(word.lower(), 0) + title.count(word.lower())
+                    X = word.lower()
+                    Y = title.count(word.lower())
+                    counts[X] = counts.get(X, 0) + Y
 
         after = data['data']['after']
         if after:
@@ -30,15 +33,3 @@ def count_words(subreddit, word_list, after=None, counts=None):
                 print(f"{word}: {count}")
     else:
         return None
-
-# Testing the function
-if __name__ == "__main__":
-    import sys
-
-    if len(sys.argv) < 3:
-        print("Usage: {} <subreddit> <list of keywords>".format(sys.argv[0]))
-        print("Ex: {} programming 'python java javascript'".format(sys.argv[0]))
-    else:
-        subreddit = sys.argv[1]
-        word_list = sys.argv[2].split()
-        count_words(subreddit, word_list)
