4f73e88b5c7f306294abc85d747deb4a317a0d04
AsiaEasa <asiaeasa89@gmail.com>
2024-05-07 09:58:11 -0700

    done

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -7,17 +7,17 @@ def recurse(subreddit, hot_list=[], after=None):
     """queries the Reddit API"""
     URL = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=100"
     HEAD = {'User-Agent': 'Hi/0.'}
-    P = {'after': A} if A else {}
+    params = {'after': after} if after else {}
 
-    R = requests.get(URL, headers=HEAD, params=P)
-    if R.status_code == 200:
-        D = R.json()
-        PO = D['data']['children']
-        if PO:
-            hot_list.extend([post['data']['title'] for post in PO])
-            A = D['data']['after']
-            if A:
-                return recurse(subreddit, hot_list, A)
+    r = requests.get(URL, headers=HEAD, params=params)
+    if r.status_code == 200:
+        data = r.json()
+        posts = data['data']['children']
+        if posts:
+            hot_list.extend([post['data']['title'] for post in posts])
+            after = data['data']['after']
+            if after:
+                return recurse(subreddit, hot_list, after)
             else:
                 return hot_list
         else:
