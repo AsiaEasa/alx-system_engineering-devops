c462486e7a3e1782963eae6c76ed0eb134db5418
AsiaEasa <asiaeasa89@gmail.com>
2024-05-07 09:33:09 -0700

    done

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,11 +7,11 @@ def number_of_subscribers(subreddit):
     """queries the Reddit API"""
     if not subreddit or type(subreddit) is not str:
         return 0
-    url = f"http://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Hi/0.0'}
-    r = requests.get(url, headers=headers)
-    if r.status_code == 200:
-        r = r.json()
+    URL = f"http://www.reddit.com/r/{subreddit}/about.json"
+    HEAD = {'User-Agent': 'Hi/0.0'}
+    R = requests.get(URL, headers=HEAD)
+    if R.status_code == 200:
+        R = R.json()
     else:
         return 0
-    return r['data']['subscribers']
+    return R['data']['subscribers']
