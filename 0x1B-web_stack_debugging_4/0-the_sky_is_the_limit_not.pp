26e8ad3506fb7c0990ce0ef9b88715da141c642c
AsiaEasa <asiaeasa89@gmail.com>
2024-05-16 10:43:57 -0700

    done

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,13 @@
+#using ApacheBench
+
+exec { 'change_default_ulimit':
+  command  => 'sed -i "s/15/2000/" /etc/default/nginx',
+  provider => 'shell',
+  notify   => Exec['restart_nginx_service']
+}
+
+exec { 'restart_nginx_service':
+  command     => 'sudo service nginx restart',
+  provider    => 'shell',
+  refreshonly => true
+}
